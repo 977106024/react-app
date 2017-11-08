5fc6bbae427f84278fe15e4c03153a7bb1e29e50
977106024 <977106024@qq.com>
2017-11-09 00:04:35 +0800

    新建leanCloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,13 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'a0RRL8DFj0fxYeB34yVQVfRW-gzGzoHsz';
+var APP_KEY = 'NUSTj9GhLqpQcPzcLACmVnfc';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY,
+
+  // 启用美国节点
+  region: 'us'
+
+
+});
